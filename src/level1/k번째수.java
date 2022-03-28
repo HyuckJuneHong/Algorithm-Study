ac7cfe12c0327eaa81a9c207c061f888bc8f0788
HyuckJuneHong <jjj8337721@naver.com>
2022-03-28 16:21:59 +0900

    Initial commit

---
+++ src/level1/k번째수.java
@@ -0,0 +1,22 @@
+package level1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class k번째수 {
+    class Solution {
+        public int[] solution(int[] array, int[][] commands) {
+            int[] answer = new int[commands.length];;
+            int[] result;
+
+            for(int i=0; i<commands.length; i++){
+                result = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+                Arrays.sort(result);
+                answer[i] = result[commands[i][2]-1];
+            }
+
+            return answer;
+        }
+    }
+}
