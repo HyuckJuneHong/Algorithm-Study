dabb02a5d03ff38aea865ae4476460450b1d125f
HyuckJuneHong <jjj8337721@naver.com>
2022-04-09 17:33:55 +0900

    codingTest

--- src/level2/sort/가장큰수Queue.java
+++ src/level2/sort/가장큰수Queue.java
@@ -5,26 +5,23 @@ import java.util.PriorityQueue;
 
 public class 가장큰수Queue {
     public String solution(int[] numbers) {
-        PriorityQueue<String> pq = new PriorityQueue<String>(new Comparator<String>() {
-            @Override
-            public int compare(String s1, String s2) {
-                return Integer.parseInt(s2 + s1) - Integer.parseInt(s1 + s2);
-            }
-        });
-        String ans = "";
 
-        for (int num : numbers) {
-            pq.offer(String.valueOf(num));
+        String answer = "";
+        PriorityQueue<String> numberPq = new PriorityQueue<>(
+                (o1, o2) -> Integer.parseInt(o2 + o1) - Integer.parseInt(o1 + o2));
+
+        for(int num : numbers){
+            numberPq.offer(String.valueOf(num));
         }
 
-        while (!pq.isEmpty()) {
-            ans += pq.poll();
+        while(!numberPq.isEmpty()){
+            answer += numberPq.poll();
         }
 
-        if (ans.charAt(0) == '0')
+        if(answer.charAt(0) == '0'){
             return "0";
-            //if (Integer.parseInt(ans) == 0)
-            //    return "0";
-        else return ans;
+        }
+
+        return answer;
     }
 }
