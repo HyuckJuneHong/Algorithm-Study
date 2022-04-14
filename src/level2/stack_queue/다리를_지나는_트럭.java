d2395ce0d8d50471e42643f4cfa4aca4ed5cd019
홍혁준 <jjj8337721@naver.com>
2022-04-14 20:24:04 +0900

    다리를 지나는 트럭

--- src/level2/stack_queue/다리를_지나는_트럭.java
+++ src/level2/stack_queue/다리를_지나는_트럭.java
@@ -5,8 +5,8 @@ import java.util.Queue;
 
 public class 다리를_지나는_트럭 {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int answer = 0;
-        int totalWeight = 0;
+        int answer = 0, totalWeight = 0;
+
         Queue<Integer> bridgeQ = new LinkedList<>();
         Queue<Integer> readyQ = new LinkedList<>();
 
