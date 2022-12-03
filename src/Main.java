730d53b420ad1affd4ba0f177299b7c4f331c9f2
홍혁준 <jjj8337721@naver.com>
2022-12-03 23:47:56 +0900

    feat: num

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,49 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.lang.reflect.Array;
+import java.util.*;
 
+/*
+[0][0] [0][1] [0][2] [0][3] [0][4]
+[1][0] [1][1] [1][2] [1][3] [1][4]
+[2][0] [2][1] [2][2] [2][3] [2][4]
+[3][0] [3][1] [3][2] [3][3] [3][4]
+[4][0] [4][1] [4][2] [4][3] [4][4]
+
+        [i-1][j]
+[i][j-1]  [i][j][i][j+1]
+        [i+1][j]
+ */
 public class Main {
     static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder stringBuilder = new StringBuilder();
-    static int[] arr;
 
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(bufferedReader.readLine());
-        arr = new int[n+1];
-        int cnt = 0;
-        Arrays.fill(arr, 0);
+        String[] s = new String[n];
 
-        for(int i=2; i<=n; i++){
-            if(arr[i]==0){
-                cnt++;
-                for(int j=i; j<=n; j+=i){
-                    arr[j] = 1;
-                }
+        int[][] arr = new int[n+2][n+2];
+        int max = Integer.MIN_VALUE;
+
+        for(int i=1; i<=n; i++){
+            s[i-1] = bufferedReader.readLine();
+            StringTokenizer stringTokenizer = new StringTokenizer(s[i-1], " ");
+            for(int j=1; j<=n; j++){
+                arr[i][j] = Integer.parseInt(stringTokenizer.nextToken());
             }
         }
 
-
+        int cnt = 0;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                int num = arr[i][j];
+                if(num > arr[i-1][j] && num > arr[i+1][j]
+                    && num > arr[i][j-1] && num > arr[i][j+1]){
+                    cnt++;
+                }
+            }
+        }
         stringBuilder.append(cnt);
         System.out.println(stringBuilder);
         bufferedReader.close();
