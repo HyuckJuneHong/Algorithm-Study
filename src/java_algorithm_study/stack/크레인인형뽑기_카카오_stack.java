6ee8026a9d6baf50752858d09976245fb85d72a1
홍혁준 <jjj8337721@naver.com>
2022-12-11 17:26:13 +0900

    feat: Stack

--- src/java_algorithm_study/stack/크레인인형뽑기_카카오_stack.java
+++ src/java_algorithm_study/stack/크레인인형뽑기_카카오_stack.java
@@ -1,61 +1,56 @@
+package java_algorithm_study.stack;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
-public class Main {
+public class 크레인인형뽑기_카카오_stack {
     static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder stringBuilder = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(bufferedReader.readLine());
         String[] s = new String[n];
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++)
             s[i] = bufferedReader.readLine();
-        }
 
         Stack<Integer>[] stacks = new Stack[n];
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++)
             stacks[i] = new Stack<>();
-        }
-        for(int i=0; i<n; i++){
-            StringTokenizer stringTokenizer = new StringTokenizer(s[n-i-1]);
-            for(int j=0; j<n; j++){
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer stringTokenizer = new StringTokenizer(s[n - i - 1]);
+            for (int j = 0; j < n; j++) {
                 int num = Integer.parseInt(stringTokenizer.nextToken());
-                if(num != 0){
-                    stacks[j].push(num);
-                }
+                if (num != 0) stacks[j].push(num);
             }
         }
 
         int m = Integer.parseInt(bufferedReader.readLine());
-        String move =  bufferedReader.readLine();
-        int[] moves = new int[m];
+        String move = bufferedReader.readLine();
         StringTokenizer stringTokenizer = new StringTokenizer(move);
-        for(int i=0; i<m; i++){
+
+        int[] moves = new int[m];
+        for (int i = 0; i < m; i++)
             moves[i] = Integer.parseInt(stringTokenizer.nextToken());
-        }
 
         int cnt = 0;
         Stack<Integer> stack = new Stack<>();
-        for(int i : moves){
-            if(!stacks[i-1].isEmpty()){
-                if(!stack.isEmpty()){
-                    if (stack.peek() == stacks[i-1].peek()){
+        for (int i : moves) {
+            if (!stacks[i - 1].isEmpty()) {
+                if (!stack.isEmpty()) {
+                    if (stack.peek() == stacks[i - 1].peek()) {
                         cnt++;
                         stack.pop();
-                        stacks[i-1].pop();
-                    }
-                    else if (stack.peek() != stacks[i-1].peek()){
-                        stack.push(stacks[i-1].pop());
-                    }
-                }else{
-                    stack.push(stacks[i-1].pop());
-                }
+                        stacks[i - 1].pop();
+                    } else if (stack.peek() != stacks[i - 1].peek()) stack.push(stacks[i - 1].pop());
+                } else stack.push(stacks[i - 1].pop());
             }
         }
 
-        stringBuilder.append(cnt*2);
+        stringBuilder.append(cnt * 2);
         System.out.println(stringBuilder);
         bufferedReader.close();
     }
